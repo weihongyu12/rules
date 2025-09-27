d1936f968bb86dc99a636beb4e07d20b56c3903a
weihongyu12 <weihongyu12@outlook.com>
2025-09-27 21:56:07 +0800

    重新整理bootstrap scss规则

--- stylelint/bootstrap-scss.js
+++ stylelint/bootstrap-scss.js
@@ -6,6 +6,45 @@ module.exports = {
   ],
   'files': '**/*.scss',
   'rules': {
+    // stylelint-config-recommended
+    'block-no-empty': true,
+    'color-no-invalid-hex': true,
+    'custom-property-no-missing-var-function': true,
+    'declaration-block-no-duplicate-custom-properties': true,
+    'declaration-block-no-duplicate-properties': [
+      true,
+      {
+        ignore: ['consecutive-duplicates-with-different-syntaxes'],
+      },
+    ],
+    'declaration-block-no-shorthand-property-overrides': true,
+    'font-family-no-duplicate-names': true,
+    'font-family-no-missing-generic-family-keyword': true,
+    'function-calc-no-unspaced-operator': true,
+    'function-linear-gradient-no-nonstandard-direction': true,
+    'keyframe-block-no-duplicate-selectors': true,
+    'keyframe-declaration-no-important': true,
+    'media-feature-name-no-unknown': true,
+    'named-grid-areas-no-invalid': true,
+    'no-descending-specificity': true,
+    'no-duplicate-at-import-rules': true,
+    'no-duplicate-selectors': true,
+    'no-empty-source': true,
+    'no-invalid-double-slash-comments': true,
+    'no-irregular-whitespace': true,
+    'property-no-unknown': true,
+    'selector-anb-no-unmatchable': true,
+    'selector-pseudo-class-no-unknown': true,
+    'selector-pseudo-element-no-unknown': true,
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignore: ['custom-elements'],
+      },
+    ],
+    'string-no-newline': true,
+    'unit-no-unknown': true,
+
     // stylelint-config-standard
     'alpha-value-notation': [
       'percentage',
@@ -113,45 +152,6 @@ module.exports = {
         ignoreValues: ['box', 'inline-box'],
       },
     ],
-
-    // stylelint-config-recommended
-    'block-no-empty': true,
-    'color-no-invalid-hex': true,
-    'custom-property-no-missing-var-function': true,
-    'declaration-block-no-duplicate-custom-properties': true,
-    'declaration-block-no-duplicate-properties': [
-      true,
-      {
-        ignore: ['consecutive-duplicates-with-different-syntaxes'],
-      },
-    ],
-    'declaration-block-no-shorthand-property-overrides': true,
-    'font-family-no-duplicate-names': true,
-    'font-family-no-missing-generic-family-keyword': true,
-    'function-calc-no-unspaced-operator': true,
-    'function-linear-gradient-no-nonstandard-direction': true,
-    'keyframe-block-no-duplicate-selectors': true,
-    'keyframe-declaration-no-important': true,
-    'media-feature-name-no-unknown': true,
-    'named-grid-areas-no-invalid': true,
-    'no-descending-specificity': true,
-    'no-duplicate-at-import-rules': true,
-    'no-duplicate-selectors': true,
-    'no-empty-source': true,
-    'no-invalid-double-slash-comments': true,
-    'no-irregular-whitespace': true,
-    'property-no-unknown': true,
-    'selector-anb-no-unmatchable': true,
-    'selector-pseudo-class-no-unknown': true,
-    'selector-pseudo-element-no-unknown': true,
-    'selector-type-no-unknown': [
-      true,
-      {
-        ignore: ['custom-elements'],
-      },
-    ],
-    'string-no-newline': true,
-    'unit-no-unknown': true,
 
     // stylelint-config-recommended-scss
     'annotation-no-unknown': null,
